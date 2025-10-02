d856e01474b0dd5fcd88d89c39cb0a8cb3c56fed
Elangovan1350 <elangovan2019miss@gmail.com>
2025-10-02 21:03:38 +0530

    change1

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client";
 import { withAccelerate } from "@prisma/extension-accelerate";
-import { env } from "bun";
 
 const prisma = new PrismaClient().$extends(withAccelerate());
 
