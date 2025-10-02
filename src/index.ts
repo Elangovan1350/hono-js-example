38c0d4d1257d53a0a90397b5c1a76d7db695e803
Elangovan1350 <elangovan2019miss@gmail.com>
2025-10-02 20:57:00 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { env } from "bun";
+
+const prisma = new PrismaClient().$extends(withAccelerate());
+
+const app = new Hono();
+
+app.get("/:name/:age/:location", (c) => {
+  const elo = c.req.param();
+  console.log(elo);
+
+  return c.json(elo);
+});
+app.post("/", async (c) => {
+  const age = await c.req.json();
+  const user = await prisma.user.create({
+    data: {
+      name: "Alice",
+      email: "alice@pr.io",
+      posts: {
+        create: { title: "Join us for Prisma Day 2020" },
+      },
+    },
+  });
+  console.log(user);
+
+  return c.json({ message: user });
+});
+
+export default app;
